4179ddd0197524f66b50043c36194662ae8fe754
Kajal <kajalpatil158@gmail.com>
2021-04-01 15:12:42 +0530

    [Kajal Patil]Added: Regex For Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("well Come In User Registration....");
 		Scanner scanner = new Scanner(System.in);
-		// Checking validity of first name
+		// Checking valid first name
 		System.out.println("Enter First Name-");
 		String firstName = scanner.next();
 		Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]");
@@ -19,7 +19,7 @@ public class UserRegistration {
 		else
 			System.out.println("Its InValid First Name");
 		
-		// Checking validity of Last name
+		// Checking valid Last name
 				System.out.println("Enter Last Name-");
 				String lastName = scanner.next();
 				Pattern pattern_lastName = Pattern.compile("[A-Z]{1}[a-z]");
@@ -29,6 +29,18 @@ public class UserRegistration {
 					System.out.println("Its Valid Last Name");
 				else
 					System.out.println("Its InValid Last Name");
+				
+				// Checking valid Email
+				System.out.println("Enter Email-");
+				String email = scanner.next();
+				Pattern pattern_email = Pattern.compile(
+						"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+				Matcher matcher_email = pattern.matcher(email);
+				boolean matches_email = matcher_email.find();
+				if (matches_email)
+					System.out.println("Its Valid Email");
+				else
+					System.out.println("Its InValid Email");
 
 	}
 }
